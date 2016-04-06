168fca2df0cfd61574c00ca8e564ada03be781da
Panayotis Matsinopoulos <panayotis.matsinopoulos@simplebusiness.co.uk>
2016-04-06 19:00:43 +0100

    Added dotenv to avoid committing pusher keys to git

--- kafka_consumer.rb
+++ kafka_consumer.rb
@@ -1,3 +1,6 @@
+require 'dotenv'
+Dotenv.load
+
 require "kafka"
 require 'pusher'
 kafka = Kafka.new(seed_brokers: ["localhost:9092"])
@@ -7,19 +10,19 @@ consumer = kafka.consumer(group_id: "my-consumer")
 
 # It's possible to subscribe to multiple topics by calling `subscribe`
 # repeatedly.
-consumer.subscribe("test_ack")
+consumer.subscribe("domain_model_ack")
 
 # This will loop indefinitely, yielding each message in turn.
 consumer.each_message do |message|
   pusher_client = Pusher::Client.new(
-      app_id: 'app_id',
-      key: 'key',
-      secret: 'secret',
+      app_id: ENV['TEST_RUBY_KAFKA_PUSHER_APP_ID'],
+      key: ENV['TEST_RUBY_KAFKA_PUSHER_KEY'],
+      secret: ENV['TEST_RUBY_KAFKA_PUSHER_SECRET'],
       cluster: 'eu',
       encrypted: true
   )
 
-  pusher_client.trigger('test_ack', 'my_event', {
+  pusher_client.trigger('domain_model_ack', 'my_event', {
       message: message.value
   })
 end
